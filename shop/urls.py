cb941b66731c7a85b9fc36361dd85310deb4fca8
Cheriffndeh <ndehhermann2527@yahoo.com>
2024-06-03 01:09:54 +0100

    update of the user home page and profile

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
     path('submit_measurements/<int:dress_id>/', views.submit_measurements, name='submit_measurements'),
     path('tailor_work/<int:tailor_id>/', views.tailor_work, name='tailor_work'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
